dfc7d07fde85b0a3c1e303d9d9eaa31db60d52f2
Durga Kishore <chitturidurgakishore@gmail.com>
2026-03-23 19:20:04 +0530

    Logo issue

--- app/Http/Controllers/Api/Admin/AdminCMSController.php
+++ app/Http/Controllers/Api/Admin/AdminCMSController.php
@@ -35,7 +35,7 @@ class AdminCMSController extends Controller
     {
         $filename = time() . '_' . Str::random(10) . '.' . $file->getClientOriginalExtension();
 
-        $path = $file->storeAs("public/media/$folder", $filename);
+        $path = $file->storeAs("media/$folder", $filename);
 
         return str_replace('public/', 'storage/', $path);
     }
