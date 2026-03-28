02472d02ff664fb204b12ec385071fa316af0623
Durga Kishore <chitturidurgakishore@gmail.com>
2026-03-28 19:37:51 +0530

    Fixed all tables softdeletes

--- app/Models/DocumentVerification.php
+++ app/Models/DocumentVerification.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 class DocumentVerification extends Model
 {
-    use SoftDeletes;
+
     protected $fillable = [
         'employer_id',
         'document_name',
--- app/Models/GeneratedCv.php
+++ app/Models/GeneratedCv.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 class GeneratedCv extends Model
 {
-    use SoftDeletes;
+
     public function jobSeeker()
     {
         return $this->belongsTo(JobSeeker::class);
