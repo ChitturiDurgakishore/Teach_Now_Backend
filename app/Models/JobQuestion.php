cd0934cb37b15a11d18b4e971ec2a6f33e4da5d6
Durga Kishore <chitturidurgakishore@gmail.com>
2026-03-28 18:27:32 +0530

    real link added

--- app/Models/JobQuestion.php
+++ app/Models/JobQuestion.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 class JobQuestion extends Model
 {
-    use SoftDeletes;
+
    public $fillable = [
     'job_id',
     'question_type',
