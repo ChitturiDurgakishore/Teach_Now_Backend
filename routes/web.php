7c4903b0c6233fc287996b745e5ed8f345575bae
Durga Kishore <chitturidurgakishore@gmail.com>
2026-04-01 11:13:13 +0530

    Fixed logout function

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', function () {
+    return response()->json([
+        'status' => false,
+        'message' => 'Please login to access this resource'
+    ], 401);
+})->name('login');
