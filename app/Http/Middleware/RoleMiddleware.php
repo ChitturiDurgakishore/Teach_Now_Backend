ff52bff1bb390ed54d9085e9ec8ea30298eece49
Durga Kishore <chitturidurgakishore@gmail.com>
2026-04-01 13:19:39 +0530

    Fixed multiple issues

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -16,7 +17,7 @@ class RoleMiddleware
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
         $user = $request->user();
-
+        Log::info("RoleMiddleware: User ID {$user->id} with role '{$user->role}' is trying to access a route requiring roles: " . implode(', ', $roles));
         if (!$user) {
             return response()->json([
                 'message' => 'Unauthenticated'
