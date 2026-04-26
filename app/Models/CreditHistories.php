deccc9bb6a40076f3e580095bc14b4181bee0b2c
Durga Kishore <chitturidurgakishore@gmail.com>
2026-04-26 12:41:51 +0530

    Added Subscription usage API

--- app/Models/CreditHistories.php
+++ app/Models/CreditHistories.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CreditHistories extends Model
 {
-    public $table='credit_histories';
+    public $table = 'credit_histories';
     public $fillable = [
         'job_id',
         'employer_id',
@@ -14,4 +14,24 @@ class CreditHistories extends Model
         'subscription_id',
         'type'
     ];
+
+    public function job()
+    {
+        return $this->belongsTo(\App\Models\Job::class, 'job_id');
+    }
+
+    public function recruiter()
+    {
+        return $this->belongsTo(\App\Models\EmployerUser::class, 'recruiter_id');
+    }
+
+    public function employer()
+    {
+        return $this->belongsTo(\App\Models\Employer::class, 'employer_id');
+    }
+
+    public function subscription()
+    {
+        return $this->belongsTo(\App\Models\Subscription::class, 'subscription_id');
+    }
 }
